2301a5a9797b2cc52bc1960f1e202e5372c510f6
cywqs <37000563+florian-kaiser@users.noreply.github.com>
2018-05-27 21:09:00 +0200

    disable quickedit at startup
    implemented function to disable QuickEdit (selecting text) in console. Solves the problem of a freezed progressbar when clicking the window.

---
+++ TinyNvidiaUpdateChecker/ConsoleQuickEdit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace TinyNvidiaUpdateChecker
+{
+    public static class ConsoleQuickEdit
+    {
+        const uint ENABLE_QUICK_EDIT = 0x0040;
+        const int STD_INPUT_HANDLE = -10;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr GetStdHandle(int nStdHandle);
+
+        [DllImport("kernel32.dll")]
+        static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
+
+        [DllImport("kernel32.dll")]
+        static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
+
+        internal static bool DisableQuickEdit()
+        {
+            IntPtr consoleH = GetStdHandle(STD_INPUT_HANDLE);
+            uint mode;
+            if (!GetConsoleMode(consoleH, out mode))
+            {
+                return false;
+            }
+            mode &= ~ENABLE_QUICK_EDIT;
+            if (!SetConsoleMode(consoleH, mode))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
