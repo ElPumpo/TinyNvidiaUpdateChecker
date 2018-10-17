7cb14472341f19830c75592faeb324fae4fd6203
Alessandro Attard Barbini <11708634+aleab@users.noreply.github.com>
2018-10-17 19:05:34 +0200

    Added '--install-components' command line argument
    The '--install-components' command line argument allows the user to specify which driver components they want to install.
    
    For instance, if they'd like to install PhysX and HDAudio in addition to the required driver package, they can now launch TinyNvidiaUpdateChecker with '--install-components=PhysX,HDAudio'.

---
+++ TinyNvidiaUpdateChecker/AnonymousEqualityComparer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TinyNvidiaUpdateChecker
+{
+    public class AnonymousEqualityComparer<T> : IEqualityComparer<T>
+    {
+        private readonly Func<T, T, bool> equals;
+        private readonly Func<T, int> getHashCode;
+
+        public AnonymousEqualityComparer(Func<T, T, bool> equals, Func<T, int> getHashCode)
+        {
+            this.equals = equals;
+            this.getHashCode = getHashCode;
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return this.getHashCode(obj);
+        }
+
+        public bool Equals(T x, T y)
+        {
+            return this.equals(x, y);
+        }
+    }
+}
