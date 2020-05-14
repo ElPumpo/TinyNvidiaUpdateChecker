a1807cc57101207d5306e2c38748e28c55438229
ElPumpo <samo_kol@hotmail.com>
2020-05-14 11:25:39 +0200

    Don't need a copy of license in every file
    Ok maybe I do need but I don't care, there's no value to this code anyways.. right?

--- TinyNvidiaUpdateChecker/Handlers/LibaryHandler.cs
+++ TinyNvidiaUpdateChecker/Handlers/LibaryHandler.cs
@@ -4,23 +4,6 @@ using System.IO;
 
 namespace TinyNvidiaUpdateChecker.Handlers
 {
-    /*
-    TinyNvidiaUpdateChecker - Check for NVIDIA GPU driver updates!
-    Copyright (C) 2016-2019 Hawaii_Beach
-    
-    This program Is free software: you can redistribute it And/Or modify
-    it under the terms Of the GNU General Public License As published by
-    the Free Software Foundation, either version 3 Of the License, Or
-    (at your option) any later version.
-
-    This program Is distributed In the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty Of
-    MERCHANTABILITY Or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License For more details.
-
-    You should have received a copy Of the GNU General Public License
-    along with this program.  If Not, see <http://www.gnu.org/licenses/>.
-    */
 
     class LibaryHandler
     {
